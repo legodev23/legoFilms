e41db6886cb7b37495ce7804c61d0db544e17480
legodev23 <leonardo.gomez23m@gmail.com>
2020-11-18 03:16:45 -0400

    Hello World

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1>Hello world! my name is Leonardo.</h1>
+      <style jsx>{`
+        h1 {
+          font-family: Ubuntu;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default index;
